9b5bc999a2d935e6a5734346db9a06a6c315da6f
Azraf-Faheem <azraf.faheem.42152@gmail.com>
2023-10-04 07:11:38 +0600

    css added

---
+++ resources/js/vue/addItemForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="addItem">
+      <input type="text" v-model="newItem" @keyup.enter="addItem" placeholder="Add a new item">
+      <button @click="addItem">Add</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newItem: "", // Store the input value here
+      };
+    },
+    methods: {
+      addItem() {
+        // Emit an event to notify the parent component about the new item
+        this.$emit("add-item", this.newItem);
+        // Clear the input field after adding the item
+        this.newItem = "";
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Scoped styles for the AddItemForm component */
+  .addItem {
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+  }
+  
+  input[type="text"] {
+    flex: 1;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+  
+  button {
+    margin-left: 10px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,10 +1,42 @@
 <template>
-    <div>
-        Welcome to TODOLIST
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title"> Welcome to TODOLIST</h2>
+        </div>
     </div>
 </template>
+
 <script>
-export default{
-    
+import addItemForm from "./addItemForm"
+import listView from "./listView"
+
+export default {
+    components: {
+        addItemForm
+    }
 }
 </script>
+
+<style scoped>
+/* Scoped styles specific to this component */
+.todoListContainer {
+    /* Add your container styles here */
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    background-color: #f5f5f5;
+}
+
+.heading {
+    /* Add your heading styles here */
+    font-size: 24px;
+    color: #333;
+    text-align: center;
+}
+
+#title {
+    /* Add styles for the title here */
+    font-weight: bold;
+    text-decoration: underline;
+}
+</style>
